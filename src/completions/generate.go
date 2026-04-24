eebc63fa3f482c97552c1eca3a82f064b55b7125
marcin <marcin.konow@lczyk.xyz>
2026-04-24 09:44:30 +0100

    continuous refactor: src/completions/generate.go

--- src/completions/generate.go
+++ src/completions/generate.go
@@ -1,3 +1,19 @@
 package completions
 
-//go:generate go run ./cmd/generate-completions
+import _ "embed"
+
+//go:embed gitgum.bash
+var bashCompletion string
+
+//go:embed gitgum.fish
+var fishCompletion string
+
+//go:embed gitgum.zsh
+var zshCompletion string
+
+// CompletionTemplates contains the embedded shell completion scripts
+var CompletionTemplates = map[string]string{
+	"bash": bashCompletion,
+	"fish": fishCompletion,
+	"zsh":  zshCompletion,
+}
