3a71a5090a36de5e61f4c046d417129dd799950f
marcin <marcin.konow@lczyk.xyz>
2026-04-24 16:47:49 +0100

    continuous refactor: src/commands/status_test.go

--- src/commands/status_test.go
+++ src/commands/status_test.go
@@ -1,1 +1,27 @@
 package commands
+
+import (
+	"testing"
+
+	"github.com/lczyk/assert"
+	"github.com/lczyk/gitgum/src/internal/temp_repo"
+)
+
+func TestStatusCommand_NotInGitRepo(t *testing.T) {
+	temp_repo.ChdirTempDir(t)
+
+	cmd := &StatusCommand{}
+	err := cmd.Execute(nil)
+
+	assert.That(t, err != nil, "should error when not in git repo")
+	assert.ContainsString(t, err.Error(), "not inside a git repository")
+}
+
+func TestStatusCommand_InGitRepo(t *testing.T) {
+	temp_repo.InitTempRepo(t)
+
+	cmd := &StatusCommand{}
+	err := cmd.Execute(nil)
+
+	assert.NoError(t, err, "should succeed in git repo")
+}
