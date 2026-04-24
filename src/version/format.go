e77cf274154ca2d423861259311793deb142d0b9
marcin <marcin.konow@lczyk.xyz>
2026-04-24 13:52:38 +0100

    continuous refactor: src/version/format.go

--- src/version/format.go
+++ src/version/format.go
@@ -2,10 +2,6 @@ package version
 
 import "strings"
 
-func GetFullVersion() string {
-	return FormatVersion(Version, CommitSHA, BuildDate, BuildInfo)
-}
-
 func FormatVersion(version, commitSHA, buildDate, buildInfo string) string {
 	if commitSHA != "" {
 		version += "+" + commitSHA[:min(7, len(commitSHA))]
