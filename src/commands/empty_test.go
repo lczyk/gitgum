b0193a2c0eabf524de77c471bfe867bcc7c2508a
marcin <marcin.konow@lczyk.xyz>
2026-04-24 12:46:10 +0100

    continuous refactor: src/commands/empty_test.go

--- src/commands/empty_test.go
+++ src/commands/empty_test.go
@@ -8,6 +8,16 @@ import (
 	"github.com/lczyk/gitgum/src/internal/temp_repo"
 )
 
+func TestEmptyCommand_NotInGitRepo(t *testing.T) {
+	temp_repo.ChdirTempDir(t)
+
+	cmd := &EmptyCommand{}
+	err := cmd.Execute(nil)
+
+	assert.That(t, err != nil, "should error when not in git repo")
+	assert.ContainsString(t, err.Error(), "repository")
+}
+
 func TestEmptyCommand_NoUpstream(t *testing.T) {
 	temp_repo.InitTempRepo(t)
 
