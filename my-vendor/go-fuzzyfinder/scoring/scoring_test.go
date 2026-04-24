9bf2bd4afd0b03b06d6401720d5d7a391e4f0f13
marcin <marcin.konow@lczyk.xyz>
2026-04-24 18:56:59 +0100

    continuous refactor: my-vendor/go-fuzzyfinder/scoring/scoring_test.go

--- my-vendor/go-fuzzyfinder/scoring/scoring_test.go
+++ my-vendor/go-fuzzyfinder/scoring/scoring_test.go
@@ -5,24 +5,23 @@ import "testing"
 func TestCalculate(t *testing.T) {
 	t.Parallel()
 
-	cases := map[string]struct {
-		s1, s2    string
-		willPanic bool
-	}{
-		"must not panic":  {s1: "foo", s2: "foo"},
-		"must not panic2": {s1: "", s2: ""},
-		"must panic":      {s1: "foo", s2: "foobar", willPanic: true},
-	}
+	t.Run("no panic with equal-length strings", func(t *testing.T) {
+		t.Parallel()
+		Calculate("foo", "foo")
+	})
 
-	for _, c := range cases {
-		if c.willPanic {
-			defer func() {
-				if err := recover(); err == nil {
-					t.Error("Calculate must panic")
-				}
-			}()
-		}
-		Calculate(c.s1, c.s2)
-	}
+	t.Run("no panic with empty strings", func(t *testing.T) {
+		t.Parallel()
+		Calculate("", "")
+	})
+
+	t.Run("panics when s2 longer than s1", func(t *testing.T) {
+		t.Parallel()
+		defer func() {
+			if recover() == nil {
+				t.Error("Calculate must panic when len(s2) > len(s1)")
+			}
+		}()
+		Calculate("foo", "foobar")
+	})
 }
-
