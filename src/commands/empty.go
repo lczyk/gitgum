e480adebe8d70f0a630bc29bfb45a0d705d9edac
marcin <marcin.konow@lczyk.xyz>
2026-04-24 11:00:31 +0100

    continuous refactor: src/commands/empty.go

--- src/commands/empty.go
+++ src/commands/empty.go
@@ -15,25 +15,24 @@ func (e *EmptyCommand) Execute(args []string) error {
 
 	currentBranch, err := internal.GetCurrentBranch()
 	if err != nil {
-		return fmt.Errorf("error getting current branch: %w", err)
+		return fmt.Errorf("getting current branch: %w", err)
 	}
 
 	upstream, err := internal.GetCurrentBranchUpstream()
 	if err != nil {
-		return fmt.Errorf("error getting upstream branch: %w", err)
+		return fmt.Errorf("getting upstream: %w", err)
 	}
 
 	ahead, err := internal.IsBranchAheadOfRemote(currentBranch, upstream)
 	if err != nil {
-		return fmt.Errorf("error checking if branch is ahead of remote: %w", err)
+		return fmt.Errorf("checking ahead status: %w", err)
 	}
 	if ahead {
 		return fmt.Errorf("refusing to create empty commit: branch '%s' is ahead of remote '%s'", currentBranch, upstream)
 	}
 
-	message := "chore: empty commit"
-	if err := internal.RunCommandWithOutput("git", "commit", "--allow-empty", "-m", message); err != nil {
-		return fmt.Errorf("failed to create empty commit: %w", err)
+	if err := internal.RunCommandWithOutput("git", "commit", "--allow-empty", "-m", "chore: empty commit"); err != nil {
+		return fmt.Errorf("creating empty commit: %w", err)
 	}
 
 	fmt.Printf("Created empty commit on branch '%s'.\n", currentBranch)
@@ -44,7 +43,7 @@ func (e *EmptyCommand) Execute(args []string) error {
 	}
 	if confirmed {
 		if err := internal.RunCommandWithOutput("git", "push"); err != nil {
-			return fmt.Errorf("failed to push: %w", err)
+			return fmt.Errorf("pushing: %w", err)
 		}
 		fmt.Printf("Pushed to remote.\n")
 	} else {
