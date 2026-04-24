cb0545349ffdec6662c358c0513b264ef4590e14
marcin <marcin.konow@lczyk.xyz>
2026-04-24 13:55:42 +0100

    continuous refactor: src/completions/generate.go

--- src/completions/generate.go
+++ src/completions/generate.go
@@ -17,17 +17,16 @@ var fishCompletion string
 //go:embed gitgum.zsh
 var zshCompletion string
 
-var templates = map[string]string{
-	"bash": bashCompletion,
-	"fish": fishCompletion,
-	"zsh":  zshCompletion,
-}
-
 const placeholder = "__GITGUM_CMD__"
 
 // Render returns the completion script for the given shell with the command
 // name substituted in.
 func Render(shell, cmdName string) (string, error) {
+	templates := map[string]string{
+		"bash": bashCompletion,
+		"fish": fishCompletion,
+		"zsh":  zshCompletion,
+	}
 	content, ok := templates[shell]
 	if !ok {
 		shells := slices.Sorted(maps.Keys(templates))
